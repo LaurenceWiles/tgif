dc088084c37612a9993baddfdcd0049934c317b5
LaurenceWiles <140092148+LaurenceWiles@users.noreply.github.com>
2024-02-15 18:22:00 +0100

    feat: added least engaged and most engaged tables to senate attendance page

--- script/senate-attendance.js
+++ script/senate-attendance.js
@@ -5,10 +5,10 @@ fetch('https://api.propublica.org/congress/v1/116/senate/members.json', {
 .then(response => response.json()) 
 .then(json =>{
   let members = json.results[0].members;
-  console.log(members)
   membersArr = [...members]
-  console.log(membersArr);
   atAGlanceTable(membersArr);
+  leastEngagedTable(membersArr)
+  mostEngagedTable(membersArr);
   
   } ) 
 .catch(err => console.log(err));
@@ -46,3 +46,68 @@ const atAGlanceTable = (arr) => {
   document.getElementById('independent_loyalty').innerHTML = Math.round(indyAvLoyalty * 100) /100;
 
 } 
+
+const leastEngagedTable = (arr) => {
+
+  let sorted = arr.sort((a,b) => (a.missed_votes > b.missed_votes) ? 1: ((b.missed_votes > a.missed_votes) ? -1 : 0));
+  let tenPercent = Math.round(arr.length / 10);
+  let leastEngaged = [];
+  for (let i = sorted.length - 1; i > (sorted.length - 1) - tenPercent; i--) {
+    leastEngaged.push(sorted[i]);
+  }
+  for (let i = (sorted.length - 1) - tenPercent; i >= 0; i--) {
+    if (sorted[i].missed_votes === leastEngaged[9].missed_votes) {
+      leastEngaged.push(sorted[i]);
+    }
+  }
+
+  const tbody = document.getElementById('least_engaged_tbody')
+  for (let i = 0; i < leastEngaged.length; i++) {
+    
+    const tr = document.createElement("tr");
+    tbody.appendChild(tr);
+
+    const name = tr.insertCell(0);
+    name.innerHTML = leastEngaged[i].first_name + " " + leastEngaged[i].last_name;
+
+    const nrMissedVotes = tr.insertCell(1);
+    nrMissedVotes.innerHTML = leastEngaged[i].missed_votes;
+
+    const percentMissed = tr.insertCell(2);
+    percentMissed.innerHTML = leastEngaged[i].missed_votes_pct;
+  }
+
+}
+
+const mostEngagedTable = (arr) => {
+
+  let sorted = arr.sort((b,a) => (a.missed_votes > b.missed_votes) ? 1: ((b.missed_votes > a.missed_votes) ? -1 : 0));
+  let tenPercent = Math.round(arr.length / 10);
+  let mostEngaged = [];
+  for (let i = sorted.length - 1; i > (sorted.length - 1) - tenPercent; i--) {
+    mostEngaged.push(sorted[i]);
+  }
+  for (let i = (sorted.length - 1) - tenPercent; i >= 0; i--) {
+    if (sorted[i].missed_votes === mostEngaged[9].missed_votes) {
+      mostEngaged.push(sorted[i]);
+    }
+  }
+
+  const tbody = document.getElementById('most_engaged_tbody')
+  for (let i = 0; i < mostEngaged.length; i++) {
+    
+    const tr = document.createElement("tr");
+    tbody.appendChild(tr);
+
+    const name = tr.insertCell(0);
+    name.innerHTML = mostEngaged[i].first_name + " " + mostEngaged[i].last_name;
+
+    const nrMissedVotes = tr.insertCell(1);
+    nrMissedVotes.innerHTML = mostEngaged[i].missed_votes;
+
+    const percentMissed = tr.insertCell(2);
+    percentMissed.innerHTML = mostEngaged[i].missed_votes_pct;
+  }
+
+}
+
